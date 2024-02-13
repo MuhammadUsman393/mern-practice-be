7a352cd447f4b0ccae2f782cde920c97ef164c16
MuhammadUsman393 <m.usman.usman393@gmail.com>
2024-02-13 17:12:35 +0500

    default route

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const port = 8008;
 //Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+app.use("/", (req, res) => {
+  res.send({ msg: "OK" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
